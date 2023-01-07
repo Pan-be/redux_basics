c01d354607bae75f05efec4830b8d54622687639
Konrad <konradbieniek@gmail.com>
2023-01-07 11:54:23 +0100

    discover actions types

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require("redux")
 
-const counterReducer = (state, action) => {
-	return {
-		counter: state.counter + 1,
+const counterReducer = (state = { counter: 0 }, action) => {
+	if (action.type === "increment") {
+		return {
+			counter: state.counter + 1,
+		}
 	}
+	if (action.type == "decrement") {
+		return {
+			counter: state.counter - 1,
+		}
+	}
+	return state
 }
 
 const store = redux.createStore(counterReducer)
@@ -14,3 +22,5 @@ const counterSubscriber = () => {
 }
 
 store.subscribe(counterSubscriber)
+
+store.dispatch({ type: "decrement" })
